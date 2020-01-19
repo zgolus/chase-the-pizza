64bad86256c0bde1dc2554d81e0c68fa14466388
Jakub Zgoliński <jakub@zgolinski.net>
2020-01-19 11:39:47 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.startCountdown(10)
+    info.changeScoreBy(1)
+    burger.setPosition(Math.randomRange(10, 150), Math.randomRange(10, 110))
+})
+let burger: Sprite = null
+scene.setBackgroundColor(9)
+let shark = sprites.create(img`
+. . . . . . . . . . . . . c c f f f . . . . . . . . . . . . . . 
+. . . . . . . . . . . . c d d b b f . . . . . . . . . . . . . . 
+. . . . . . . . . . . c d d b b f . . . . . . . . . . . . . . . 
+. . . . . . . . . . f c c b b c f . . . . . . . . . . . . c c c 
+. . . . f f f f f f c c c c c c f f . . . . . . . . . c c b b c 
+. . f f b b b b b b b b b b b b b c f f f . . . . . c d b b c . 
+f f b b b b b b b b b c b c b b b b c c c f f . . c d d b b f . 
+f b c b b b b b f f b b c b c b b b c c c c c f f f d b b f . . 
+f b b b 1 1 1 1 f f 1 b c b c b b b c c c c c c c b b b c f . . 
+. f b 1 1 1 1 1 1 1 1 b b b b b b c c c c c c c c c b c c f . . 
+. . f c c c 3 3 c c 1 1 b b b b c c c c c c c c f f f b b c f . 
+. . . f c 1 3 1 c 1 1 1 b b b c c c c c b d b c . . . f b b f . 
+. . . . f 3 3 c 1 1 1 c b b b f d d d d d c c . . . . . f b b f 
+. . . . . f f 1 1 1 1 f b d b b f d d c c . . . . . . . . f f f 
+. . . . . . . c c c c c f b d b b f c . . . . . . . . . . . . . 
+. . . . . . . . . . . . . f f f f f . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+burger = sprites.create(img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`, SpriteKind.Food)
+controller.moveSprite(shark)
